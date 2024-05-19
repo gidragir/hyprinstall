2bf0fedea4e5e03af4cb7d232cf121a1a53fdf4b
gidragir <ilyabalter1@gmail.com>
2024-05-20 00:07:19 +0500

    asd

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ makepkg -si
 cd $base
 rm -rf /tmp/yay
 
-sudo pacman -S xorg-xhost bash-completion hypridle hyprlock hyprpaper
+sudo pacman -S xorg-xhost bash-completion hypridle hyprlock hyprpaper nautilus
 xhost + local:
 
 yay -S --needed --answerclean All --answerdiff None aylurs-gtk-shell microsoft-edge-stable telegram-desktop visual-studio-code-bin grub-customizer
