563fa700b20881f3d1022fe26cb3a82ff08e7e77
Gidragir <ilyabalter1@gmail.com>
2024-05-20 00:18:44 +0500

    gnome

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ makepkg -si
 cd $base
 rm -rf /tmp/yay
 
-sudo pacman -S xorg-xhost bash-completion nautilus dart-sass fd brightnessctl slurp wl-clipboard asusctl power-profiles-daemon
+sudo pacman -S xorg-xhost bash-completion nautilus dart-sass fd brightnessctl slurp wl-clipboard asusctl power-profiles-daemon gnome-bluetooth-3.0
 xhost + local:
 
 sudo systemctl enable power-profiles-daemon.service
